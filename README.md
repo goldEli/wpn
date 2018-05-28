a895ee618e96d7b17eaf9ceb05cbda83dc98f7fd
SF-2465 <miaoyu2009@qq.com>
2018-05-28 17:56:46 +0800

    optimize some code

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ## log
 
+* pm2 list
+* pm2 stop all
+
 ## reference
 
 * [nodejs服务端开发（Express+Mysql）---小k博客 http://htmlk.cn](https://github.com/htmlk/express)
--- servers/users/users.js
+++ servers/users/users.js
@@ -1,54 +1,29 @@
 var $sql = require('./sql');
 var URL = require('url');
 var UUID = require('node-uuid');
-var md = {};
 var db = require('../../config/db');
 var resultObj = {data:[],msg:'success',status:0}
+var base = require('../base');
+
+var md = {};
 
 // check all
-md.checkall = function(req, res, next) {
-    db($sql.all,function (err, data) {
-        if(err){
-          console.log('[SELECT ERROR] - ',err.message);
-          return;
-        }
-        console.log('check all users success');  
-        //把搜索值输出
-        resultObj.data = data
-        res.send(resultObj);
-    });
+md.checkall = function(req, res) {
+    base.check({sql:$sql.all,req, res})
 }
 
 // delete by id
 md.deleteById = function(req, res, next) {
 	var params = URL.parse(req.url, true).query;
-	console.log(params.id)
-	var sql = $sql.deleteById + `'` + params.id + `'`
-	console.log(sql)
-	db(sql,function (err, result) {
-		if(err){
-		console.log('[DELETE ERROR] - ',err.message);
-		return;
-		}      
-		console.log('delete by id success');   
-		resultObj.data = null
-		res.send(resultObj);
-	});
+	var sql = $sql.deleteById + `'` + params.id + `'`;
+	base.deleteById({sql, req, res});
 }
 
 // add
 md.add = function(req, res, next) {
     var params = URL.parse(req.url, true).query;
-    var uuid = UUID.v4();
-    var addSqlParams = [uuid, params.name];
-    db($sql.insert,function (err, result) {
-        if(err){
-         console.log('[INSERT ERROR] - ',err.message);
-         return;
-        }      
-        resultObj.data = null
-        res.send(resultObj);       
-    },addSqlParams);
+    var addSqlParams = [params.name];
+    base.add({sql:$sql.insert, req, res, addSqlParams});
 }
 
 module.exports = md;
