10240a961466a140befd50e3385692e44729227a
SF-2465 <miaoyu2009@qq.com>
2018-05-29 11:29:13 +0800

    optimze some code

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var usersServers = require('../servers/users/users');
+var UUID = require('node-uuid');
+var URL = require('url');
+var usersServers = require('../servers/usersServers/usersServers');
 
 router.get('/', function(req, res, next) {
-  usersServers.checkall(req, res)
+  usersServers.queryallUsers(function(data){
+    res.send(data);
+  })
 });
 
 // add
 router.get('/add', function(req, res, next){
-  usersServers.add(req, res)
+  var params = URL.parse(req.url, true).query;
+  var uuid = UUID.v4().replace(/-/g,'');
+  var addParams = [uuid, params.name]
+  usersServers.addUser(addParams, function(data){
+    res.send(data);
+  })
 })
 
 // del
 router.get('/del', function(req, res, next){
-  usersServers.deleteById(req, res)
+  var params = URL.parse(req.url, true).query;
+  var id = params.id;
+  usersServers.deleteById(id, function(data){
+    res.send(data)
+  })
 })
 
 
